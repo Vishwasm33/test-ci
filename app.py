a49de489aeacb2fbc98d515f9796f76f6edfacb2
VISHWAS M <121951898+Vishwasm33@users.noreply.github.com>
2026-03-16 11:59:34 +0530

    Change app host to localhost in app.py

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    
     return "Hello, DevOps Students!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="127.0.0.1", port=5000)
